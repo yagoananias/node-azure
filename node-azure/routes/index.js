fae77d6c88ec00bb43ccb7242645b26a9852f02e
Yago Ananias <yago_souza92@yahoo.com.br>
2021-07-17 22:07:11 -0300

    Testando layout

--- node-azure/routes/index.js
+++ node-azure/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node Azure Yago' });
+  res.render('index', { title: 'Node Azure Yago teste' });
 });
 
 module.exports = router;
